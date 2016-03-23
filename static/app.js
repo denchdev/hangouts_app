196fafd6086c46de0e525f0dcec0ed88b38852e0
denromani4 <denromani4@gmail.com>
2016-03-24 01:56:35 +0200

    fix app

--- static/app.js
+++ static/app.js
@@ -1,43 +1,4 @@
-/*
-* Copyright (c) 2011 Google Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not
-* use this file except in compliance with the License. You may obtain a copy of
-* the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations under
-* the License.
-*/
 var serverPath = '//mejoprome-1.appspot.com/';
-
-// The functions triggered by the buttons on the Hangout App
-function countButtonClick() {
-  // Note that if you click the button several times in succession,
-  // if the state update hasn't gone through, it will submit the same
-  // delta again.  The hangout data state only remembers the most-recent
-  // update.
-  console.log('Button clicked.');
-  var value = 0;
-  var count = gapi.hangout.data.getState()['count'];
-  if (count) {
-    value = parseInt(count);
-  }
-
-  console.log('New count is ' + value);
-  // Send update to shared state.
-  // NOTE:  Only ever send strings as values in the key-value pairs
-  gapi.hangout.data.submitDelta({'count': '' + (value + 1)});
-}
-
-function resetButtonClick() {
-  console.log('Resetting count to 0');
-  gapi.hangout.data.submitDelta({'count': '0'});
-}
 
 var forbiddenCharacters = /[^a-zA-Z!0-9_\- ]/;
 function setText(element, text) {
@@ -46,30 +7,12 @@ function setText(element, text) {
       '';
 }
 
-function getMessageClick() {
-  console.log('Requesting message from main.py');
-  var http = new XMLHttpRequest();
-  http.open('GET', serverPath);
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var jsonResponse = JSON.parse(http.responseText);
-      console.log(jsonResponse);
-
-      var messageElement = document.getElementById('message');
-      setText(messageElement, jsonResponse['message']);
-    }
-  }
-  http.send();
-}
-
 function updateStateUi(state) {
-  var countElement = document.getElementById('count');
-  var stateCount = state['count'];
-  if (!stateCount) {
-    setText(countElement, 'Probably 0');
-  } else {
-    setText(countElement, stateCount.toString());
-  }
+  console.log('updateStateUi');
+  var countElement = document.getElementById('time');
+  var stateCount = state['time'];
+  console.log('stateCount');
+
 }
 
 function updateParticipantsUi(participants) {
@@ -78,30 +21,49 @@ function updateParticipantsUi(participants) {
   setText(participantsListElement, participants.length.toString());
 }
 
+function startLesson() {
+  console.log('Button clicked');
+  var value =0;
+  var time = gapi.hangout.data.getState()['time'];
+  console.log('time', time);
+  if (time) {
+    value = parseInt(time);
+  }
+  gapi.hangout.data.submitDelta({'time': '' + (time + 1)});
+}
+
+function render() {
+  var lesson = {};
+  lesson.user = {};
+  lesson.user.name = 'Peter';
+  lesson.user.role = 'Student';
+  lesson.subject = 'Antique Greece History';
+  lesson.hourlyRate = 10.0;
+  lesson.id = gapi.hangout.getStartData();
+
+  var body = document.getElementById('body');
+  var infoDiv = document.createElement('div');
+  infoDiv.innerHTML = '<h4>' + lesson.subject + ' (' + lesson.hourlyRate + '$/hr)<h4>' +
+  '<h5>' + lesson.user.name + ' ' + lesson.user.role + '</h5>' +
+  '<h5>' + 'Denis' + ' ' + 'teacher' + '</h5>' +
+  '<h4>Lesson time: <p id="time>"0:00</p> ($0.00)</h4>' +
+  '<h1>lesson - ' + lesson.id + '</h1>';
+
+  var controlDiv = document.createElement('div');
+  controlDiv.innerHTML = '<button onClick="startLesson()">Start lesson</button>';
+
+  body.appendChild(infoDiv);
+  body.appendChild(controlDiv);
+
+  gapi.hangout.data.setValue('time', '0');
+}
+
 // A function to be run at app initialization time which registers our callbacks
 function init() {
   console.log('Init app.');
 
   var apiReady = function(eventObj) {
     if (eventObj.isApiReady) {
-      console.log('API is ready');
-      console.log('jquery', $());
-      jQuery.ajax({
-             type: "GET",
-             url: "https://api.instagram.com/v1/users/1234",
-
-             dataType: "text",
-             success: function (data, status, jqXHR) {
-                 console.log('data', data);
-             },
-
-             error: function (jqXHR, status) {
-                 console.log('error', jqXHR);
-                 console.log('errorStatus', status);
-             }
-           });
-
-
       gapi.hangout.data.onStateChanged.add(function(eventObj) {
         updateStateUi(eventObj.state);
       });
@@ -109,8 +71,10 @@ function init() {
         updateParticipantsUi(eventObj.participants);
       });
 
-      updateStateUi(gapi.hangout.data.getState());
-      updateParticipantsUi(gapi.hangout.getParticipants());
+      var start_data = gapi.hangout.getStartData();
+      console.log('start data', start_data);
+      console.log('start data', JSON.parse(start_data));
+      render();
 
       gapi.hangout.onApiReady.remove(apiReady);
     }
